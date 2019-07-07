3fd6ffe5bce87e1100a21e1bfaf13c6422b1987b
marvinHo1 <myuanho1@gmail.com>
2019-07-07 14:48:57 -0700

    seed database with information

---
+++ client/src/index.jsx
@@ -0,0 +1,8 @@
+import React from'react';
+import ReactDOM from 'react-dom';
+import Popular from './components/popular.jsx';
+
+
+
+
+ReactDOM.render(< Popular />, document.getElementById('test'))
---
+++ database/seed.js
@@ -0,0 +1,39 @@
+var db = require('./index.js');
+var faker = require('faker');
+var mongoose = require('mongoose');
+
+var popularDish = new mongoose.Schema({
+    name: String,
+    image: String,
+    price: Number,
+    revCount: Number,
+    phoCount:Number
+  });
+
+
+  var Dish = db.model('Dish', popularDish);
+
+  var seed = function seedData(){ 
+
+    for (var i = 0 ; i < 100; i++) {
+        var names = faker.lorem.word();
+        var image = `https://popularlist.s3-us-west-1.amazonaws.com/munch${i}.jpg`;
+        var ranNum = function getRandomInt(max) {
+            return Math.floor(Math.random() * Math.floor(max));
+            };
+
+        var popDish = new Dish ({
+            name: names,
+            image: image,
+            price: ranNum(35),
+            revCount: ranNum(30),
+            phoCount: ranNum(200),
+        })
+
+        popDish.save(function (err) {
+        if (err) return console.error(err);
+        console.log('Saved!')
+            });
+        }
+    }
+    seed()
