2c352b9eb866d0fc6d2fd83147c6ca279d80216c
Marvin Ho <marvinho@Marvins-MacBook-Pro.local>
2019-07-12 16:06:22 -0700

    Mapped out images and information

--- client/src/components/photoList.jsx
+++ client/src/components/photoList.jsx
@@ -9,27 +9,37 @@ const Photolist = (props) => {
     {console.log(props.data[0].image)}
     return (
         <div className = {style.containerForPopularDishImage}>
-            {/* mapping function start */}
+            {props.data.map(pic =>
+          <div className = {style.test}>
             <div className = {style.containerForImagesAndContent}>
                 <div className = {style.popularDishDivContainer}>
-                    <img className ={style.popularDishImages} src= {props.data[0].image} ></img>
-                    <span className = {style.popularDishPrice}>${props.data[0].price}.00</span>
+                    <img className ={style.popularDishImages} src= {pic.image} key= {pic._id} ></img>
+                    <span className = {style.popularDishPrice}>${pic.price}.00</span>
                 </div>
                 <div className = {style.popularDishName}>
-                    {props.data[0].name}
+                    {pic.name}
                 </div>
                 <div className = {style.containerForPhotoCountAndReview}>
-                    <small className = {style.popularDishPhotoCount }>{props.data[0].phoCount} Photos</small>
+                    <small className = {style.popularDishPhotoCount }>{pic.phoCount} Photos</small>
                     <small className = {style.popularDishImportantDot}>•</small>
-                    <small className = {style.popularDishReviewCount}>{props.data[0].revCount} Reviews</small>
+                    <small className = {style.popularDishReviewCount}>{pic.revCount} Reviews</small>
                 </div>
             </div>
-            {/* mapping function end */}
+          </div>
+            )}
         </div>
     )
 }
 
 // {props.data.map(pic => <img className ={style.foodImages} src = {pic.image} key = {pic._id}></img>)}
        
+// image: "https://popularlist.s3-us-west-1.amazonaws.com/munch3.jpg"
+// name: "ullam"
+// phoCount: 2
+// price: 7
+// revCount: 3
+// __v: 0
+// _id: "5d24f4c6ee0020e1f0a649ff"
+
 export default Photolist
 
