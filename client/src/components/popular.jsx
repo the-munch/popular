cbda9b07ae7a2649706ada64f96ba673d073ee05
Marvin Ho <myuanHo1@gmail.com>
2019-07-13 15:24:25 -0700

    Connected Client to Server and Database

--- client/src/components/popular.jsx
+++ client/src/components/popular.jsx
@@ -1,18 +1,42 @@
 import React from 'react'
+import $ from "jquery";
 import PhotoList from './photoList.jsx';
 import PopHeader from './header.jsx';
-import style from '../style.css'
+import style from '../style.css';
 
 
 class Popular extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            list: []
+        }
 
         this.scrollLeft = this.scrollLeft.bind(this)
         this.scrollRight = this.scrollRight.bind(this)
+        // this.testcall = this.testcall.bind(this)
     }
 
+componentDidMount() {
+    $.ajax({
+    method: 'GET',
+    url: '/munch-popular',
+    success: (data) => {
+        // console.log(data)
+        this.setState(() => {
+            return {list: data};
+          });
+    }
+ });
+}
+
+// (state) => {
+//     return {quantity: state.quantity + 1};
+//   });
+
+// var state = Object.assign({}, this.state);
+// this.state = data;
+// this.setState(state);
 scrollLeft() {
     document.getElementById('containerForContent').scrollLeft -= 500;
 }
@@ -23,17 +47,15 @@ scrollRight() {
 
 
 render() {
-    // {console.log(this.scrollLeft)}
     return(
         <div className = {style.moduleContainer}>  
         <div><PopHeader
         scrollLeft = {this.scrollLeft} 
         scrollRight = {this.scrollRight}
         /></div>
-        <div><PhotoList data = {this.props.data}/> </div>
+        <div><PhotoList data = {this.state.list}/> </div>
          </div>
         )
-    {console.log(Photolist)}
     }
 }
 
